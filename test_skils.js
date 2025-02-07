dcac3ea3bbafe14e6d313da49111a63e1a76768a
DM1-N1 <69199890+DM1-N1@users.noreply.github.com>
2025-02-07 00:51:05 +0000

    Added the files I had been working in in the library

---
+++ test_skils.js
@@ -0,0 +1,8 @@
+let button = document.querySelector('#button1')
+
+button.addEventListener('click',buttonpress)
+
+function buttonpress() {
+    console.log("YO")
+    window.alert("Your a smart man")
+}
