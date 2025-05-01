2e32e095e378081a26dfceccd2ae57015a4e8b33
David Mamattah <D.Mamattah@hotmail.com>
2025-05-01 20:46:42 +0100

    Made a function to get the url of the current tab the user is viewing

--- test_skils.js
+++ test_skils.js
@@ -1,11 +1,25 @@
 let button = document.querySelector('#button1')
+let urlbutton = document.querySelector('#urlbutton')
 
-button.addEventListener('click',geturl)
+
+function buttonpress() {
+    console.log("YO")
+    window.alert("Your a smart man")
+}
+
+
 
 function geturl() {
     currenturl=window.location.href
-    console.log(currenturl)
+    console.log("This is the ", currenturl)
     return currenturl
 }
+
+function addEventListener() {
+    button.addEventListener('click',buttonpress);
+    urlbutton.addEventListener('click',geturl);
+}
+
+addEventListener()
 
 // ignore this
